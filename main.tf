e4175e77f49107ec3e8fe38e0be506752df4c6df
ross <ross.mailinglists2@gmail.com>
2021-09-09 16:47:59 +0200

    changed ssh key-pair behaviour to import-only

--- main.tf
+++ main.tf
@@ -91,7 +91,8 @@ resource "aws_instance" "default" {
   user_data                   = var.user_data
   iam_instance_profile        = join("", aws_iam_instance_profile.default.*.name)
   associate_public_ip_address = var.associate_public_ip_address
-  key_name                    = signum(length(var.ssh_key_pair)) == 1 ? var.ssh_key_pair : module.ssh_key_pair.key_name
+  #key_name                    = signum(length(var.ssh_key_pair)) == 1 ? var.ssh_key_pair : module.ssh_key_pair.key_name
+  key_name                    = aws_key_pair.imported.key_name
   subnet_id                   = var.subnet
   monitoring                  = var.monitoring
   private_ip                  = concat(var.private_ips, [""])[min(length(var.private_ips), count.index)]
@@ -122,10 +123,14 @@ resource "aws_instance" "default" {
   )
 }
 
-##
-## Create keypair if one isn't provided
-##
 
+# import the public key instead of generating it with the key-pair module
+resource "aws_key_pair" "imported" {
+  key_name   = module.this.id
+  public_key = file(var.ssh_key_pair_path)
+}
+
+/*
 module "ssh_key_pair" {
   source                = "cloudposse/key-pair/aws"
   version               = "0.18.0"
@@ -134,7 +139,7 @@ module "ssh_key_pair" {
   generate_ssh_key      = var.generate_ssh_key_pair
 
   context = module.this.context
-}
+}*/
 
 resource "aws_eip" "default" {
   count             = local.count_default_ips
